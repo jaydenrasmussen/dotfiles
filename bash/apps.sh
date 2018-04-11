a73ee824979c3d29d55109d95142b76706e69b72
Jayden Rasmussen <jayden.rasmussen@me.com>
2018-04-11 14:25:22 -0600

    added font installers

--- bash/apps.sh
+++ bash/apps.sh
@@ -15,6 +15,9 @@ echo "                 Installing Applications            "
 echo "----------------------------------------------------"
 brew install caskroom/cask/brew-cask
 brew cask install adapter appcleaner caffeine discord dropbox dupeguru etcher evernote firefox flash go2shell google-chrome google-play-music-desktop-player hermes itsycal keka notational-velocity notion onyx qlcolorcode qlmarkdown qlstephen slack smcfancontrol spotify sublime-text transmission unetbootin veracrypt virtualbox virtualbox-extension-pack vlc
+brew tap caskroom/fonts
+brew cask install font-roboto font-roboto-condensed font-roboto-mono \
+ font-roboto-slab font-inter-ui
 brew cask cleanup
 
 # macos
