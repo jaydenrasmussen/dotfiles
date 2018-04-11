68d7a877699989b981512a8aebdac28b69459bf4
Jayden Rasmussen <jayden.rasmussen@me.com>
2018-04-11 13:27:59 -0600

    added dupeguru

--- bash/apps.sh
+++ bash/apps.sh
@@ -14,7 +14,7 @@ echo "----------------------------------------------------"
 echo "                 Installing Applications            "
 echo "----------------------------------------------------"
 brew install caskroom/cask/brew-cask
-brew cask install adapter appcleaner caffeine discord dropbox etcher evernote firefox flash go2shell google-chrome google-play-music-desktop-player hermes itsycal keka notational-velocity notion onyx qlcolorcode qlmarkdown qlstephen slack smcfancontrol spotify sublime-text transmission unetbootin veracrypt virtualbox virtualbox-extension-pack vlc
+brew cask install adapter appcleaner caffeine discord dropbox dupeguru etcher evernote firefox flash go2shell google-chrome google-play-music-desktop-player hermes itsycal keka notational-velocity notion onyx qlcolorcode qlmarkdown qlstephen slack smcfancontrol spotify sublime-text transmission unetbootin veracrypt virtualbox virtualbox-extension-pack vlc
 brew cask cleanup
 
 # macos
